447bd2abe585eac7970182aa26f36fb79337ff80
Matt Regner <regnerm@live.unc.edu>
2024-12-22 14:47:16 -0500

    Added updated Luminal Cohort Results script

---
+++ scripts/Luminal_Cohort_Results-updates-Revised-updated.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name Luminal_Cohort_Results-updates-Revised-updated
+#SBATCH -c 32
+#SBATCH --mem 128g
+#SBATCH --partition allnodes 
+#SBATCH --output scripts/Luminal_Cohort_Results-updates-Revised-updated.out 
+
+# Run R script in docker container 
+singularity exec --bind /datastore --home $PWD docker://regnerm/scbreast_2023:1.2.0 Rscript scripts/Luminal_Cohort_Results-updates-Revised-updated.R 
